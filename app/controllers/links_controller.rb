8fc5e04404c839aeebb1bb0d81cd58fc344957f6
diego <dgcuenca@gmail.com>
2023-11-30 16:36:54 -0500

    Display enter URLs

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,8 @@
 class LinksController < ApplicationController
   def index
-    @links = Link.all
+    # @links = Link.order(created_at: :desc)
+    # implement as a shorcut because it is common this is comming from the model using scope
+    @links = Link.recent_first
   end
 
   def create
@@ -8,6 +10,11 @@ class LinksController < ApplicationController
     if @link.save
       redirect_to root_path
     else
+      #we have to have the instance variable before render the index action because in the 
+      #index view we are rendering @links and if we dont have we are going to get an error
+      #also we can call the index action:
+      #index
+      @links = Link.recent_first
       render :index, status: :unprocessable_entity
     end
   end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,6 @@
 class Link < ApplicationRecord
+  #in order to reuse it
+  scope :recent_first, -> { order(created_at: :desc)}
+
   validates :url, presence: true
 end
