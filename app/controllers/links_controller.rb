b2167dd73ae927aba53e1534b3c5546c5e916b87
diego <dgcuenca@gmail.com>
2023-11-30 15:58:26 -0500

    Process information for create a new url in db

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,4 +2,19 @@ class LinksController < ApplicationController
   def index
     @links = Link.all
   end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:url)
+  end
 end
