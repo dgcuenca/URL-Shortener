3c361e82847ebcee5a351a4d01d905ff2f583832
diego <dgcuenca@gmail.com>
2023-12-01 12:26:25 -0500

    Encode urls enter by user

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,4 +3,10 @@ class Link < ApplicationRecord
   scope :recent_first, -> { order(created_at: :desc)}
 
   validates :url, presence: true
+
+  # to_param is a inbuild method of rails that give you the id here we are overwriting it
+  def to_param
+    ShortCode.encode(id)
+  end
+
 end
