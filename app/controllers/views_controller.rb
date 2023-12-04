a88c67de274309cb536e711c5467730c908eac20
diego <dgcuenca@gmail.com>
2023-12-04 11:58:35 -0500

    Display the number of views a link has

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -2,6 +2,9 @@ class ViewsController < ApplicationController
   before_action :set_link, only: [:show]
 
   def show
+    #its necessary if for example you want to detect boots
+    @link.views.create(ip: request.ip, user_agent: request.user_agent)
+    
     redirect_to @link.url, allow_other_host: true
   end
 end
