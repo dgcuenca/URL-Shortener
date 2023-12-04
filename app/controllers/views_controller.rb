b9747a1348c925be7518ce1bb0c640dfcdbb7668
diego <dgcuenca@gmail.com>
2023-12-04 14:44:39 -0500

    Redirect to home page when there is a bad link

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,6 +1,10 @@
 class ViewsController < ApplicationController
   before_action :set_link, only: [:show]
 
+  rescue_from ActiveRecord::RecordNotFound do
+    redirect_to root_path, alert: "Missing link!"
+  end
+
   def show
     #its necessary if for example you want to detect boots
     @link.views.create(ip: request.ip, user_agent: request.user_agent)
